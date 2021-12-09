709a55fe41473a41cdedc06f2229640ca550c0fb
Joshua Soon <mythicalwarrior.js@gmail.com>
2021-12-09 11:41:26 +0800

    added annotate_target_chemical_probe_association

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 import target_disease
+import target_chem_probe
 import pandas as pd
 
 
-def annotate_disease_target_association(ensembl_csv, ensembl_header, disease):
+def annotate_target_disease_association(ensembl_csv, ensembl_header, disease):
     ensembl_df = pd.read_csv(ensembl_csv)
     rows, columns = ensembl_df.shape
 
@@ -10,10 +11,10 @@ def annotate_disease_target_association(ensembl_csv, ensembl_header, disease):
 
     for i in range(rows):
         ensembl_id = ensembl_df.loc[i, ensembl_header]
-        disease_target_association = \
+        target_disease_association = \
             target_disease.get_target_disease_association(ensembl_id=ensembl_id, disease_name=disease)
         # Get disease target association counts and nested list of disease target associations.
-        counts, nested_list = target_disease.extract_association_scores(disease_target_association)
+        counts, nested_list = target_disease.extract_association_scores(target_disease_association)
         average_association_score = target_disease.aggregate_association_scores(nested_list)
 
         ensembl_df_annotated.loc[i, columns+1] = average_association_score
@@ -29,4 +30,24 @@ def annotate_disease_target_association(ensembl_csv, ensembl_header, disease):
 
     return ensembl_df_annotated
 
+
+def annotate_target_chemical_probe_association(ensembl_csv, ensembl_header, drug_id_filter="yes"):
+    ensembl_df = pd.read_csv(ensembl_csv)
+    rows, columns = ensembl_df.shape
+
+    ensembl_df_annotated = ensembl_df  # Make a copy of the original ensembl_df so as to not overwrite it.
+
+    for i in range(rows):
+        ensembl_id = ensembl_df.loc[i, ensembl_header]
+        target_probe_association = target_chem_probe.get_target_chemical_probe_association(ensembl_id=ensembl_id)
+        counts, nested_list = target_chem_probe.extract_associations(target_probe_association, drug_id_filter)
+
+        ensembl_df_annotated.loc[i, columns+1] = counts
+
+        if len(nested_list) > 0:
+            for index, associations in enumerate(nested_list):
+                chem_probes_listToStr = ', '.join(map(str, associations))
+                ensembl_df_annotated.loc[i, columns + 2 + index] = chem_probes_listToStr
+
+    return ensembl_df_annotated
 
