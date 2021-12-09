ffa8ea9281ba6adce02d70b0499f8f46263d2772
Joshua Soon <mythicalwarrior.js@gmail.com>
2021-12-09 11:47:20 +0800

    use main ensemble ID csv file

--- usage_script.py
+++ usage_script.py
@@ -4,8 +4,8 @@ import main
 csv_file = "STM2020_NASH_stages_genes.csv"
 csv_file_2 = "test_ensembl_id.csv"
 
-target_disease_association_results = main.annotate_target_disease_association(csv_file_2, "Ensembl_id", "Liver")
+target_disease_association_results = main.annotate_target_disease_association(csv_file, "Ensembl_id", "Liver")
 target_disease_association_results.to_csv('target_disease_associations.tsv', sep='\t', encoding='utf-8', index=False)
 
-target_probe_association_results = main.annotate_target_chemical_probe_association(csv_file_2, "Ensembl_id")
+target_probe_association_results = main.annotate_target_chemical_probe_association(csv_file, "Ensembl_id", "no")
 target_probe_association_results.to_csv('target_chem_probe_associations.tsv', sep='\t', encoding='utf-8', index=False)
