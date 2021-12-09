dfdefa6b6741d3de7f05f174d15e24c3ac2e000a
Joshua Soon <mythicalwarrior.js@gmail.com>
2021-12-09 16:39:36 +0800

    added target_drug_association_results

--- usage_script.py
+++ usage_script.py
@@ -9,3 +9,6 @@ target_disease_association_results.to_csv('target_disease_associations.tsv', sep
 
 target_probe_association_results = main.annotate_target_chemical_probe_association(csv_file, "Ensembl_id", "no")
 target_probe_association_results.to_csv('target_chem_probe_associations.tsv', sep='\t', encoding='utf-8', index=False)
+
+target_drug_association_results = main.annotate_target_known_drug_association(csv_file, "Ensembl_id")
+target_drug_association_results.to_csv('target_drug_associations.tsv', sep='\t', encoding='utf-8', index=False)
