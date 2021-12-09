10db6506c8bc8ebff968d37f1816aa5db476ad31
Joshua Soon <mythicalwarrior.js@gmail.com>
2021-12-09 11:42:31 +0800

    added target_probe_association

--- usage_script.py
+++ usage_script.py
@@ -2,6 +2,10 @@ import main
 
 # For usage, add the full name of the csv file of Ensembl_ids to the variable csv_file.
 csv_file = "STM2020_NASH_stages_genes.csv"
+csv_file_2 = "test_ensembl_id.csv"
 
-target_disease_association_results = main.annotate_disease_target_association(csv_file, "Ensembl_id", "Liver")
-target_disease_association_results.to_csv('disease_target_associations.tsv', sep='\t', encoding='utf-8', index=False)
+target_disease_association_results = main.annotate_target_disease_association(csv_file_2, "Ensembl_id", "Liver")
+target_disease_association_results.to_csv('target_disease_associations.tsv', sep='\t', encoding='utf-8', index=False)
+
+target_probe_association_results = main.annotate_target_chemical_probe_association(csv_file_2, "Ensembl_id")
+target_probe_association_results.to_csv('target_chem_probe_associations.tsv', sep='\t', encoding='utf-8', index=False)
